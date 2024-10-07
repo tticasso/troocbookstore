8530a7ce4492a7b24f9ef4d5dbe715954038c478
LaHieu11 <lahieutx@gmail.com>
2024-10-07 21:49:41 +0700

    Login-Sign fe

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
